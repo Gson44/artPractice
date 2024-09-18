3f2db6546c93a2e96ef41264b28c9ee56141e6dd
Gerson <geric99997@gmail.com>
2024-09-17 20:18:42 -0600

    Add stop features

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 
 let playBTN = document.getElementById("startBTN")
+let stopBTN = document.getElementById("stopBTN");
+let sound = new Audio("alarm.wav");
 
 playBTN.addEventListener("click", timer)
-
+stopBTN.addEventListener("click", stopTimer)
 
 
 function timer(){
    
-    var sec = 59;
-    var min = 60;
+    var sec = 10;
+    var min = 1;
     var timer = setInterval(function(){
         document.getElementById('timer').innerHTML=min+':'+sec;
         sec--;
         if (sec < 0) {
-            sec = 60;
+            sec = 10;
             min--;
             //clearInterval(timer);
+        }
+        if(min == 0){
+           
+            sound.play()
+            clearInterval(timer);
         }
     }, 1000);
 }
 
+
+function stopTimer(){
+    sound.pause()
+}
